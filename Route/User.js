f5d46f9814e9cbaeda77a6c12e16ee6100ec5941
Ibrahim Ahmed <96918810+IbrahimAhmed413@users.noreply.github.com>
2024-11-13 14:26:49 +0500

    feat: updated and finalized

--- Route/User.js
+++ Route/User.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-// const User = require('../models/user');
+
 const postUser = require('../Controller/userController');
 
-console.log(postUser);
-router.post('/create', postUser );
+    router.post('/create', postUser );
   module.exports = router; 
